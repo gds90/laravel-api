7d3de9a6f0242ae8260f047080bf6e1982177a25
gds90 <you@example.com>
2024-03-01 14:35:23 +0100

    Milestone 1 completed

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::with('type', 'technology')->paginate(6);
+        return response()->json([
+            'success' => true,
+            'results' => $projects
+        ]);
+    }
+}
