cd4dee9728ee11795265f80691da813ae4a819aa
gds90 <you@example.com>
2024-03-05 15:13:03 +0100

    Update: added project details view

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -16,4 +16,22 @@ class ProjectController extends Controller
             'results' => $projects
         ]);
     }
+
+    public function show($slug)
+    {
+        // recupero il progetto attraverso lo slug passato come parametro
+        $project = Project::with('type', 'technology')->where('slug', $slug)->first();
+
+        // se esiste il progetto
+        if ($project) {
+            return response()->json([
+                'success' => true,
+                'project' => $project
+            ]);
+        }
+        // se non esiste
+        return response()->json([
+            'success' => false
+        ]);
+    }
 }
